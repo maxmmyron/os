a901fc14238185b68305c77d2c53416a97b80aff
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-02-03 14:39:48 +0000

    update boot

--- boot.asm
+++ boot.asm
@@ -1,21 +1,22 @@
 [org 0x7c00]
 
-mov [BOOT_DRIVE], dl
+mov si, message   ; load the source index register with the address of our message
+call print_str
 
-mov     bp, 0x8000
-mov     sp, bp
+print_str:
+  mov ah, 0Eh     ; set the function code to 0Eh. This is the function code for printing a string
 
-mov     bx, 0x9000
-mov     dh, 5
+.run:
+  loadsb          ; load the byte at the source index into al and increment si
+  cmp al, 0       ; compare al to 0 (the null terminator)
+  je .done        ; if al is 0, jump to .done
+  int 10h         ; otherwise, print the character in al
+  jmp .run        ; and jump back to .run
 
-mov     dl, [BOOT_DRIVE]
-call    disk_load
+.done:
+  jmp $           ; infinte loop
 
-mov     dx, [0x9000]
-call    print_hex
+message   db "Hello World!", 0
 
-mov     dx, [0x9000 + 512]
-call    print_hex
-
-jmp     $
-
+times 510 - ($-$$) db 0 ; create n zeros, where n is 510 (512 - 2 magic number bytes at end) minus the size of the previous code
+dw 0xaa55
