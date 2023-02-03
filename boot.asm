ab3304dfdb9ec7931e7ff5fa99e5241bc7ff0085
Max Myron <maxmmyron@gmail.com>
2023-02-03 15:19:47 +0000

    update boot

--- boot.asm
+++ boot.asm
@@ -7,8 +7,8 @@ print_str:
   mov ah, 0Eh     ; set the function code to 0Eh. This is the function code for printing a string
 
 .run:
-  loadsb          ; load the byte at the source index into al and increment si
-  cmp al, 0       ; compare al to 0 (the null terminator)
+  lodsb           ; load the byte at the source index into al and increment si
+  cmp al, 0x00    ; compare al to 0 (the null terminator)
   je .done        ; if al is 0, jump to .done
   int 10h         ; otherwise, print the character in al
   jmp .run        ; and jump back to .run
