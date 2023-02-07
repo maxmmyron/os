5fc03a97d8193e511b28faaf48d7b978b4590fb5
Max Myron <maxmmyron@gmail.com>
2023-02-07 02:09:37 +0000

    move process files

---
+++ kernel/process.h
@@ -0,0 +1,15 @@
+#ifndef _PROCESS_H
+#define _PROCESS_H
+
+#include "../cpu/types.h"
+
+struct pcb
+{
+  char *name;             // process name       8
+  u8 proc_id;             // process ID         1
+  u8 priority;            // process priority   1
+  u8 status;              // process status     1
+  int (*function)(void);  // process function   4 ?
+};
+
+#endif
