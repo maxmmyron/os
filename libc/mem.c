e274d037e87e9a4e6bdce98319246df6cd438977
Max Myron <maxmmyron@gmail.com>
2023-02-07 18:11:58 +0000

    improve malloc (i get it now kinda)

--- libc/mem.c
+++ libc/mem.c
@@ -1,5 +1,8 @@
 #include "mem.h"
 
+// internal function definitions
+void *malloc_internal(u32 size, int align, u32 *phys_addr);
+
 // a simpler implementation of memcpy, renamed such that we don't encounter
 // possible namespace collisions
 void mcpy(u8 *source, u8 *dest, int nbytes) {
@@ -13,21 +16,40 @@ void mset(u8 *dest, u8 val, u32 len) {
     for ( ; len != 0; len--) *temp++ = val;
 }
 
-u32 free_mem_addr = 0x10000;
+u32 placement_address = 0x10000;
 
-void *malloc_aligned(u32 size, int align) {
-  // if pages are aligned to 4k (0x1000)
-  if(align == 1 && (free_mem_addr & 0xfffff000)) {
-    free_mem_addr &= 0xfffff000;
-    free_mem_addr += 0x1000;
-  }
-
-  void *ptr = (void*)free_mem_addr;
-  free_mem_addr += size;  // increment pointer to next free space based on size
-                          // of malloc'd memory
+void *malloc(u32 size) {
+  void *ptr = malloc_internal(size, 0, 0);
   return ptr;
 }
 
-u32 get_real_addr() {
-  return free_mem_addr;
+void *malloc_a(u32 size) {
+  void *ptr = malloc_internal(size, 1, 0);
+  return ptr;
+}
+
+void *malloc_p(u32 size, u32 *phys_addr) {
+  void *ptr = malloc_internal(size, 0, phys_addr);
+  return ptr;
+}
+
+void *malloc_ap(u32 size, u32 *phys_addr){
+  void *ptr = malloc_internal(size, 1, phys_addr);
+  return ptr;
+}
+
+void *malloc_internal(u32 size, int align, u32 *phys_addr) {
+  if (align == 1 && (placement_address & 0xFFFFF000)) // If the address is not already page-aligned
+  {
+    // Align it.
+    placement_address &= 0xFFFFF000;
+    placement_address += 0x1000;
+  }
+  if (phys_addr)
+  {
+    *phys_addr = placement_address;
+  }
+  u32 tmp = placement_address;
+  placement_address += size;
+  return (void*) tmp;
 }
--- libc/mem.h
+++ libc/mem.h
@@ -3,12 +3,14 @@
 
 #include "../cpu/types.h"
 
-#define malloc(size) malloc_aligned(size, 1);
-
 void mcpy(u8 *source, u8 *dest, int nbytes);
 void mset(u8 *dest, u8 val, u32 len);
 
-void *malloc_aligned(u32 size, int align);
-u32 get_real_addr();
+// vanilla malloc functions. return a virtual address pointer
+void *malloc(u32 size);
+void *malloc_a(u32 size);                   // page-aligned
+// malloc functions that return a physical address pointer
+void *malloc_p(u32 size, u32 *phys_addr);
+void *malloc_ap(u32 size, u32 *phys_addr);  // page-aligned
 
 #endif
